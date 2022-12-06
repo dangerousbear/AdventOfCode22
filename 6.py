1610c4cf8766c43063a21683ff07dbaab5e61319
Björn Andersson <bjorn.aaa@gmail.com>
2022-12-06 11:50:36 +0100

    Day 6

--- 6.py
+++ 6.py
@@ -1,5 +1,5 @@
 #%%
 line = open("Input/6.txt", "r").read()
 N = 14
-print(1+next(i for i in range(N, len(line)) if len(set(line[i:i-N:-1])) == N))
+print(next(i for i in range(N, len(line)) if len(set(line[i-N:i])) == N))
 
