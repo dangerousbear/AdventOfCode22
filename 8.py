aa4299770989bfe187e9effaf5ce146f16e70cd5
Björn Andersson <bjorn.aaa@gmail.com>
2022-12-09 10:42:37 +0100

    Day 8

--- 8.py
+++ 8.py
@@ -2,16 +2,16 @@
 import numpy as np
 
 lines = open("Input/8.txt", "r").readlines()
-Nr, Nc = len(lines), len(lines[0])-1
-f = np.zeros((Nr, Nc), dtype=int)
-for i, line in enumerate(lines):
-    for j, c in enumerate(line.strip("\n")):
-        f[i, j] = int(c)
+n_rows, n_cols = len(lines), len(lines[0])-1
+f = np.zeros((n_rows, n_cols), dtype=int)
+for r in range(n_rows):
+    for c in range(n_cols):
+        f[r,c] = int(lines[r][c])
 
-# View length is 1 + index of first element not lower than the treehouse height (h)
-vl = lambda arr,h : next((i+1 for i, x in enumerate(arr) if x >= h), len(arr))
+# Viewing distance of trees in arr is 1 + index of first element not lower than the treehouse height (h)
+vd = lambda arr,h : next((i+1 for i, x in enumerate(arr) if x >= h), len(arr))
 
-# Product of N, W, S, E view lengths for height h
-sight_prod = lambda r,c,h : vl(f[:r, c][::-1],h) * vl((f[r, :c])[::-1],h) * vl(f[r, c+1:],h) * vl(f[r+1:, c],h)
+# Product of N, W, S, E view distances for height h. N & W are reversed using [::-1]
+score = lambda r,c,h : vd(f[:r, c][::-1],h) * vd((f[r, :c])[::-1],h) * vd(f[r, c+1:],h) * vd(f[r+1:, c],h)
 
-print("Answer", max( sight_prod(r,c, f[r,c]) for r in range(Nr) for c in range(Nc)))
+print(max(score(r,c, f[r,c]) for r in range(n_rows) for c in range(n_cols)))
