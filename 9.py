c7192bde4a0cbd9bb269893511710a4095fc2910
Björn Andersson <bjorn.aaa@gmail.com>
2022-12-09 11:14:36 +0100

    Day 9

--- 9.py
+++ 9.py
@@ -3,20 +3,16 @@ import numpy as np
 
 lines = open("Input/9.txt", "r").readlines()
 dir2vec = {"U" : [0,1], "R" : [1,0], "D": [0, -1], "L" : [-1, 0]}
-moves = []
-for line in lines:
-    d, v = line.strip("\n").split()
-    moves.extend([np.array(d) for _ in range(int(v))])
-
 x = [np.array([0,0]) for _ in range(10)] # The rope, head is x[0]
 visited = set()
-
-for m in moves:
-    x[0] += m
-    for next, this in zip(x,x[1:]):
-        d = next-this
-        if (abs(d).max() > 1):
-            this += np.clip(d,-1,1) # Never move more than 1 step in each dir
-    visited.add(x[-1].tostring())
+for line in lines:
+    dir, v = line.strip("\n").split()
+    for _ in range(int(v)):
+        x[0] += np.array(dir2vec[dir])
+        for next, this in zip(x,x[1:]):
+            d = next-this
+            if (abs(d).max() > 1):
+                this += np.clip(d,-1,1) # Never move more than 1 step in each dir
+        visited.add(x[-1].tostring())
 
 print(len(visited))
